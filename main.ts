d73e4def6288c799743c7a94e7812da226966be3
GitMurf <64155612+GitMurf@users.noreply.github.com>
2021-11-15 23:25:08 -0800

    Create initial PageLinkAutocompleteSuggester class extending EditorSuggest

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Plugin, PluginSettingTab, Setting, EditorSuggest, EditorPosition, TFile, EditorSuggestTriggerInfo, EditorSuggestContext, LinkCache } from 'obsidian';
 declare module "obsidian" {
     interface WorkspaceLeaf {
         containerEl: HTMLElement;
@@ -22,6 +22,7 @@ export default class MyPlugin extends Plugin {
     async onload() {
         console.log("loading plugin: " + pluginName);
         await this.loadSettings();
+        this.registerEditorSuggest(new PageLinkAutocompleteSuggester(this.app, this));
 
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
@@ -81,5 +82,86 @@ class SampleSettingTab extends PluginSettingTab {
                     this.plugin.settings.mySetting = value;
                     await this.plugin.saveSettings();
                 }));
+    }
+}
+
+class PageLinkAutocompleteSuggester extends EditorSuggest<string> {
+
+    constructor(app: App, private thisPlugin: Plugin) {
+        super(app);
+    }
+
+    addlink(linkArr: string[], myLink: string) {
+        if (!linkArr) {
+            return true;
+        } else {
+            if (!linkArr.contains(myLink)) { return true } else { return false }
+        }
+    }
+
+    onTrigger(cursor: EditorPosition, editor: Editor, file: TFile): EditorSuggestTriggerInfo | null {
+        if (cursor.ch === 0) {
+            // at beginning of line so exit
+            return null;
+        } else {
+            const curLineStr = editor.getLine(cursor.line);
+            const curLineStrMatch = curLineStr.substring(0, cursor.ch);
+            const cursorChar = curLineStrMatch.substring(curLineStrMatch.length - 1);
+            if (cursorChar !== `;`) {
+                return null;
+            } else {
+                const lastWord = curLineStrMatch.split(" ").last();
+                return {
+                    start: { line: cursor.line, ch: 0 },
+                    end: { line: cursor.line, ch: curLineStr.length - 1 },
+                    query: lastWord
+                };
+            }
+        }
+    }
+
+    getSuggestions(context: EditorSuggestContext): string[] | Promise<string[]> {
+        const queryText = context.query.substring(0, context.query.length - 1);
+        console.log(context.query);
+        console.log(queryText);
+        const mdCache = this.thisPlugin.app.metadataCache.getFileCache(context.file);
+        let allLinks: LinkCache[];
+        if (mdCache) {
+            allLinks = mdCache.links;
+        }
+        if (allLinks) {
+            let myLinks: string[] = [];
+            allLinks.forEach(eachLink => {
+                if (eachLink.displayText) {
+                    if (eachLink.displayText === eachLink.link) {
+                        if (this.addlink(myLinks, eachLink.link)) { myLinks.push(eachLink.link) }
+                    } else {
+                        if (this.addlink(myLinks, eachLink.link)) { myLinks.push(eachLink.link) }
+                        if (this.addlink(myLinks, eachLink.displayText)) { myLinks.push(eachLink.displayText) }
+                    }
+                } else {
+                    if (this.addlink(myLinks, eachLink.link)) { myLinks.push(eachLink.link) }
+                }
+            })
+            return myLinks.filter(eachLink => eachLink.toLowerCase().contains(queryText.toLowerCase()));
+        } else {
+            return null;
+        }
+    }
+
+    renderSuggestion(value: string, el: HTMLElement) {
+        el.setText(value);
+    }
+
+    selectSuggestion(value: string, event: MouseEvent | KeyboardEvent) {
+        const editor = this.context.editor;
+        const cursor = editor.getCursor();
+        const curLineStr = editor.getLine(cursor.line);
+        const curLineStrMatch = curLineStr.substring(0, cursor.ch);
+        const lastWord = curLineStrMatch.split(" ").last();
+        const lastWordCh = curLineStrMatch.indexOf(lastWord);
+        const newLink = `[[${value}]]`;
+        editor.replaceRange(newLink, { line: cursor.line, ch: lastWordCh }, { line: cursor.line, ch: lastWordCh + lastWord.length });
+        editor.setSelection({ line: cursor.line, ch: lastWordCh + newLink.length });  // place cursor between tags
     }
 }
